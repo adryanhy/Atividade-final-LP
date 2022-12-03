f4157cf5f1bc9b30d7b317f40abed73dbbc8ef0c
Adryan <100885847+adryanhy@users.noreply.github.com>
2022-12-03 14:31:43 -0300

    Update conta.h

--- conta.h
+++ conta.h
@@ -7,4 +7,20 @@ void inserir(p_conta pConta[], int posicao);
 
 void alterar(p_conta pConta[], int tamanho);
 
-void procurarConta(p_conta pConta[], int tamanho);
+int procurarConta();
+
+void listar(p_conta pConta[], int tamanho);
+
+void depositar(p_conta pConta[], int tamanho);
+
+void sacar(p_conta pConta[], int tamanho);
+
+//void imprimir (p_conta pConta[], int tamanho);
+
+//void imprimir (p_conta pConta[], int tamanho);
+
+void saldoGeral(p_conta pConta[], int tamanho);
+
+void imprimir (p_conta const *pConta, int i);
+
+//int procurarConta(p_conta pConta[]);
